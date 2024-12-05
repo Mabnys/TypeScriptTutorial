084ab0d33e07aa4c2c4cf54debee0e73d79219ea
Mamadou A. Balde <99915114+Mabnys@users.noreply.github.com>
2024-12-05 16:13:42 -0500

    Removed the timestamp
    This complete the Part of the tutorial: Next JS Tutorial for Beginners, Next’s 13 (App Router) with Typescript - Programming with Mosh

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -13,7 +13,6 @@ const UsersPage = async () => {
   return (
     <>
      <h1>Users</h1>
-     <p>{new Date().toLocaleTimeString()}</p>
     <table className='table table-bordered'>
       <thead>
         <tr>
